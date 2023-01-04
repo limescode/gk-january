b6dcc1915a5c7e394bf603310fe0acfd04379472
limescode <katechizis@gmail.com>
2023-01-04 10:13:07 +0100

    initial commit

---
+++ src/main/java/pl/limescode/gkjanuary/GkJanuaryApplication.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkjanuary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkJanuaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkJanuaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/limescode/gkjanuary/GkJanuaryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkjanuary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkJanuaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
